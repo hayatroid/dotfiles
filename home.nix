e5be27f930402a4e2c50d3e03f1fcddb54d0e3d5
hayatroid <hayatroid@gmail.com>
2025-10-16 13:12:53 +0000

    :sparkles: setup rust, node, go, etc.

--- home.nix
+++ home.nix
@@ -18,30 +18,34 @@
   home.stateVersion = "25.05"; # Please read the comment before changing.
 
   home.packages = with pkgs; [
+    docker
     fzf
+    gcc
     git
+    htmlq
+    jq
+    openssl
+    pkg-config
     vim
+    wget
+
+    nodejs
+
+    rustup
+    tombi
+
+    go
+    golangci-lint
   ];
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/hayatroid/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
   };
+
+  home.sessionPath = [
+    "$HOME/.cargo/bin"
+    "$HOME/.npm-global/bin"
+  ];
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
