af90203ce83658831a4eb9275a05fbf7a6c5bad0
hayatroid <hayatroid@gmail.com>
2025-10-19 04:08:20 +0000

    :recycle: use parameter expansion

--- home-manager/bouyomi/default.nix
+++ home-manager/bouyomi/default.nix
@@ -8,35 +8,18 @@ in
     siteFunctions = {
       # ref: https://gist.github.com/ookam/c08096f78fe7ec64922f69cea7f485fc
       bouyomi = ''
-        local input
-        if [ -n "''$1" ]; then
-          input="''$1"
-        else
-          input=$(cat)
-        fi
+        local input="''${1:-$(cat)}"
         local bytes=$(echo -n "''$input" </dev/null | wc -c)
         local win_ip=$(ip route | grep default | cut -d' ' -f3)
         printf "\x01\x00\xff\xff\xff\xff\xff\xff\x00\x00\x00\x$(printf %02x ''$((bytes & 255)))\x$(printf %02x ''$((bytes >> 8)))\x00\x00''$input" | nc -w 2 ''${win_ip:-0.0.0.0} 50001
       '';
 
       akane = ''
-        local input
-        if [ -n "''$1" ]; then
-          input="''$1"
-        else
-          input=$(cat)
-        fi
-        bouyomi "akane) ''$input"
+        bouyomi "akane) ''${1:-$(cat)}"
       '';
 
       aoi = ''
-        local input
-        if [ -n "''$1" ]; then
-          input="''$1"
-        else
-          input=$(cat)
-        fi
-        bouyomi "aoi) ''$input"
+        bouyomi "aoi) ''${1:-$(cat)}"
       '';
     };
   };
